2c83ace6fd8fdb3b6668570c6a614ef19a125f92
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2024-06-05 19:50:48 +0700

    update stop_container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+set -e
+
+# Stop the running container (if any)
+echo "Stopping all running Docker containers..."
+
 # Stop all running containers
 docker stop $(docker ps -q)
 
 # Optionally, you can also remove all stopped containers
 docker rm $(docker ps -a -q)
+
+echo "All Docker containers have been stopped and removed."

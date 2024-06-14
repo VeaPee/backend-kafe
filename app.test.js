8d8b6decb512cf52166883431f0850c17958dba1
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2024-06-14 18:20:36 +0700

    add testing

--- app.test.js
+++ app.test.js
@@ -8,6 +8,33 @@ describe("/", () => {
   it("should return a response", async () => {
     const response = await request.get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Hello World");
+  });
+});
+
+describe("/api/users/getuser", () => {
+  it("should return a response", async () => {
+    const response = await request.get("/api/users/getuser");
+    expect(response.status).toBe(401);
+  });
+});
+
+describe("/api/products", () => {
+  it("should return a response", async () => {
+    const response = await request.get("/api/products");
+    expect(response.status).toBe(401);
+  });
+});
+
+describe("/api/users/logout", () => {
+  it("should return a response", async () => {
+    const response = await request.get("/api/users/logout");
+    expect(response.status).toBe(200);
+  });
+});
+
+describe("/api/users/loggedin", () => {
+  it("should return a response", async () => {
+    const response = await request.get("/api/users/loggedin");
+    expect(response.status).toBe(200);
   });
 });
